a474573ab5b7be1c8fb8490cd3f0082d0840c612
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-14 21:40:37 -0500

    render modal using createPortal into separate div

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import ReactDOM from 'react-dom';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import JobListings from './pages/JobListings';
+import { Context } from './components/Context';
+import Home from './pages/Home';
 import JobDetails from './pages/JobDetails';
+import FilterModal from './components/modal/FilterModal';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const { isModalOpen } = useContext(Context);
 
   function toggleDarkMode() {
     setIsDarkMode(prevState => !prevState);
   }
 
   return (
-    <div className={`app ${!isDarkMode ? '' : 'dark'}`}>
-      <Navbar handleClick={toggleDarkMode} />
+    <div className={`${isDarkMode ? 'bg-bodyDark' : 'bg-body'}`}>
       <Routes>
         <Route
           exact
           path="/"
-          element={<JobListings theme={isDarkMode} />}
+          element={<Home handleClick={toggleDarkMode} theme={isDarkMode} />}
         ></Route>
         <Route
           path="/:jobId"
-          element={<JobDetails theme={isDarkMode} />}
+          element={
+            <JobDetails handleClick={toggleDarkMode} theme={isDarkMode} />
+          }
         ></Route>
       </Routes>
+
+      {isModalOpen &&
+        ReactDOM.createPortal(
+          <FilterModal theme={isDarkMode} />,
+          document.getElementById('modal-root')
+        )}
     </div>
   );
 }
